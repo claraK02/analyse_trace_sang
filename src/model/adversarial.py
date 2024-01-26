00ae3fb4b1fdfb9c0bbb1db10e8dcde5e8691cb7
Highdrien <adrien.zabban@outlook.fr>
2024-01-27 00:36:01 +0100

    chnage adv_model

--- src/model/adversarial.py
+++ src/model/adversarial.py
@@ -12,18 +12,19 @@ from src.model.basemodel import Model
 class AdversarialResNet(Model):
     def __init__(self,
                  hidden_size: int,
+                 resnet_hidden_size: int,
                  p_dropout: float,
                  background_classes: int
                  ) -> None:
         super().__init__()
-        self.fc1 = nn.Linear(in_features=512, out_features=hidden_size)
+        self.fc1 = nn.Linear(in_features=resnet_hidden_size, out_features=hidden_size)
         self.dropout = nn.Dropout(p_dropout)
         self.relu = nn.ReLU()
         self.fc2 = nn.Linear(in_features=hidden_size, out_features=background_classes)
 
     def forward(self, x: Tensor) -> Tensor:
         """
-        input: x is a tensor of shape (batch_size, 1000)
+        input: x is a tensor of shape (batch_size, resnet_hidden_size)
         output: y is a tensor of shape (batch_size, background_classes)
         """
         x = self.fc1(x)
