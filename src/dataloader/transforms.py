523d746be621475e3e8740dce193f49baefa9e78
Highdrien <adrien.zabban@outlook.fr>
2024-01-26 18:19:31 +0100

    add transforms only in the train

--- src/dataloader/transforms.py
+++ src/dataloader/transforms.py
@@ -10,20 +10,24 @@ from torchvision.transforms import (
 )
 
 
-def get_transforms(transforms_config: EasyDict) -> Compose:
+def get_transforms(transforms_config: EasyDict,
+                   mode: str
+                   ) -> Compose:
+    """ Compose transfrom if mode==train"""
     transform = []
 
-    if transforms_config.run_rotation:
-        transform.append(RandomRotation(degrees=(0, 180)))
+    if mode == 'train':
+        if transforms_config.run_rotation:
+            transform.append(RandomRotation(degrees=(0, 180)))
 
-    if transforms_config.run_hflip:
-        transform.append(RandomHorizontalFlip(p=0.5))
-    
-    if transforms_config.run_vflip:
-        transform.append(RandomVerticalFlip(p=0.5))
-    
-    if sum(transforms_config.color.values()) != 0:
-        transform.append(ColorJitter(**transforms_config.color))
+        if transforms_config.run_hflip:
+            transform.append(RandomHorizontalFlip(p=0.5))
+        
+        if transforms_config.run_vflip:
+            transform.append(RandomVerticalFlip(p=0.5))
+        
+        if sum(transforms_config.color.values()) != 0:
+            transform.append(ColorJitter(**transforms_config.color))
     
     transform.append(ToTensor())
     return Compose(transform)
