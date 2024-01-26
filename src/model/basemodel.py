396112c650c3a679fbaaad2c71c5f9b4a8ecabac
Highdrien <adrien.zabban@outlook.fr>
2024-01-26 23:32:37 +0100

    add basemodel

---
+++ src/model/basemodel.py
@@ -0,0 +1,91 @@
+from typing import Iterator, Tuple
+
+from torch import nn
+from torch.nn import Parameter
+
+
+class Model(nn.Module):
+    """ BaseModel with method for get parameter more easily """
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+    
+    def get_parameters(self) -> Iterator[Parameter]:
+        """ get parameters """
+        return self.parameters()
+    
+    def get_learned_parameters(self) -> Iterator[Parameter]:
+        """ get learned parameters """
+        for param in self.parameters():
+            if param.requires_grad:
+                yield param
+    
+    def get_name_learned_parameters(self) -> Iterator[Tuple[str, Parameter]]:
+        """ get name and parameters st param is leanable"""
+        for name, param in self.named_parameters():
+            if param.requires_grad:
+                yield name, param
+
+    def get_number_parameters(self) -> int:
+        """ get the number of parameters """
+        return sum(p.numel() for p in self.parameters())
+
+    def get_number_learnable_parameters(self) -> int:
+        """ get the number of learned parameters """
+        return sum(p.numel() for p in self.parameters() if p.requires_grad) 
+    
+    def get_dict_learned_parameters(self) -> dict[str, Parameter]:
+        """ get learned parameters in a dict to save it """
+        state_dict: dict[str, Parameter] = {}
+        for name, param in self.named_parameters():
+            if param.requires_grad:
+                state_dict[name] = param
+
+        return state_dict
+    
+    def load_dict_learnable_parameters(self,
+                                       state_dict: dict[str, Parameter],
+                                       strict: bool=True,
+                                       verbose: bool=False
+                                       ) -> None:
+        """ load only learnable param from a dict
+        # Arguments:
+        state_dict: dict[str, Parameter]
+            dict which contains the name and the parameters
+        strict: bool, default=True
+            if True, raise Error if there are some matching error
+        verbose: bool, default=True
+            if True and strict=False, print Warning Error if there are some matching error
+        """
+        loaded_keys = []
+        for name, param in self.named_parameters():
+            # if name in keys
+            if name in state_dict.keys():
+                if param.requires_grad:
+                    param = state_dict[name]
+                    loaded_keys.append(name)
+                elif strict or verbose:
+                    error_message = f'parameter:{name} is in state_dict.keys \
+                                    but this param has requires_grad=False'
+                    print_error_message(error_message, strict, verbose)
+            
+            # if name not in keys but param is requires_grad
+            elif param.requires_grad:
+                error_message = f'parameter:{name} not in state_dict.keys \
+                                but this param has requires_grad=True'
+                print_error_message(error_message, strict, verbose)
+
+        # if loaded_keys != state_dict.keys()   (we know that: state_dict.keys() c loaded_keys)
+        if len(loaded_keys) != len(state_dict):
+            missing_keys = [key for key in range(state_dict.keys()) if key not in loaded_keys]
+            error_message = f"the following parameter wasn't load: {missing_keys}"
+            print_error_message(error_message, strict, verbose)
+        
+        print('dict was successful load')
+    
+
+
+def print_error_message(error_message: str, strict: bool, verbose: bool) -> None:
+    if strict:
+        raise KeyError(error_message)
+    if verbose:
+        print(Warning(error_message))
--- src/model/resnet.py
+++ src/model/resnet.py
@@ -1,74 +1,53 @@
-from typing import Iterator
+import os
+import sys
+from os.path import dirname as up
 
 import torch
 from torch import nn, Tensor
 from torchvision import models
-from torch.nn import Parameter
 from torchvision.models.resnet import ResNet18_Weights
 
+sys.path.append(up(up(up(os.path.abspath(__file__)))))
 
-class Resnet(nn.Module):
+from src.model.basemodel import Model
+
+
+class Resnet(Model):
     def __init__(self,
                  num_classes: int,
-                 dropout_probability: float=0.5,
-                 hidden_size: int=100
                  ) -> None:   
         super(Resnet, self).__init__()
 
-        self.resnet = models.resnet18(weights=ResNet18_Weights.IMAGENET1K_V1)
+        resnet = models.resnet18(weights=ResNet18_Weights.IMAGENET1K_V1)
+        self.resnet_begin = nn.Sequential(*(list(resnet.children())[:-1]))
 
-        for param in self.resnet.parameters():
+        for param in self.resnet_begin.parameters():
             param.requires_grad = False
         
-        self.resnet.fc = torch.nn.Linear(512, num_classes)
-
+        self.fc = torch.nn.Linear(512, num_classes)
 
     def forward(self, x: Tensor) -> Tensor:
         """
-        input: x is a tensor of shape (batch_size, 3, 512, 512)
+        input: x is a tensor of shape (batch_size, 3, 128, 128)
         output: y is a tensor of shape (batch_size, num_classes)
         """
-        return self.resnet(x)
-    
-    def get_parameters(self) -> Iterator[Parameter]:
-        return self.parameters()
-    
-    def get_learned_parameters(self) -> Iterator[Parameter]:
-        for param in self.resnet.parameters():
-            if param.requires_grad:
-                yield param
+        x = self.resnet_begin(x)
+        x = x.squeeze(-1).squeeze(-1)
+        x = self.fc(x)
+        return x
 
-    def count_total_parameters(self) -> int:
-        return sum(p.numel() for p in self.parameters())
-
-    def count_trainable_parameters(self) -> int:
-        return sum(p.numel() for p in self.parameters() if p.requires_grad) 
-    
-    def get_only_learned_parameters(self) -> dict[str, Tensor]:
-        """ get only the learned parameters """
-        state_dict: dict[str, Tensor] = {}
-        for name, param in self.named_parameters():
-            if param.requires_grad:
-                state_dict[name] = param
-
-        return state_dict
-
-
-
-    
 
 if __name__ == '__main__':
+    from torch import nn
     model = Resnet(num_classes=2)
-    print("Total parameters:",model.count_total_parameters())
-    print("Trainable parameters:",model.count_trainable_parameters())
-    print(model.resnet)
-
-    for param in model.get_learned_parameters():
-        print(param)
+    print("Total parameters:", model.get_number_parameters())
+    print("Trainable parameters:", model.get_number_learnable_parameters())
+    learnable_param = model.get_dict_learned_parameters()
+    print('learnable parameters', learnable_param)
+    model.load_dict_learnable_parameters(state_dict=learnable_param, strict=True)
 
     x = torch.randn((32, 3, 128, 128))
     y = model.forward(x)
 
     print("y shape:",y.shape)
-    print("y:",y)
 
