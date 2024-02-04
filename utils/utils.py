6aaca544713592735e4cf78587b724d5cdf74d03
Highdrien <adrien.zabban@outlook.fr>
2024-02-04 19:47:56 +0100

    add load weight

--- utils/utils.py
+++ utils/utils.py
@@ -4,6 +4,8 @@ import torch
 from typing import Any, List
 from os.path import dirname as up
 
+from torch.nn import Parameter
+
 sys.path.append(up(os.path.abspath(__file__)))
 
 from src.metrics import Metrics
@@ -32,3 +34,21 @@ def get_metrics_name_for_adv(resnet_metrics: Metrics, adv_metrics: Metrics) -> L
     metrics_name = list(map(add_resnet, resnet_metrics.get_names())) + \
                    list(map(add_adv, adv_metrics.get_names()))
     return metrics_name
+
+
+def load_weights(logging_path: str,
+                 model_name: str='res',
+                 device: torch.device=torch.device('cpu'),
+                 endfile: str='.pt'
+                 ) -> dict[str, Parameter]:
+    weight_files = list(filter(lambda x: x.endswith(endfile), os.listdir(logging_path)))
+    
+    if len(weight_files) == 1:
+        weight = torch.load(os.path.join(logging_path, weight_files[0]), map_location=device)
+        return weight
+    
+    model_name_files = list(filter(lambda x: model_name in x, os.listdir(logging_path)))
+    if len(model_name_files) > 1:
+        raise FileExistsError(f'Confused by multiple weights for the {model_name} model')
+    weight = torch.load(os.path.join(logging_path, model_name_files[0]), map_location=device)
+    return weight
