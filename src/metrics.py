b6e991262af5536cfcfd1c2fe63cac00eebcddcf
Highdrien <adrien.zabban@outlook.fr>
2024-02-05 11:07:31 +0100

    add get_info

--- src/metrics.py
+++ src/metrics.py
@@ -24,9 +24,13 @@ class Metrics:
                         'f1-score': F1Score(task='multiclass',
                                             average=average,
                                             num_classes=num_classes)}
+        self.metrics_onehot = {'top k': Accuracy(task='multiclass',
+                                                 average=average,
+                                                 num_classes=num_classes,
+                                                 top_k=3)}
         
-        self.num_metrics = len(self.metrics)
-        self.metrics_name = list(self.metrics.keys())
+        self.num_metrics = len(self.metrics_onehot) + len(self.metrics)
+        self.metrics_name = list(self.metrics_onehot.keys()) + list(self.metrics.keys())
         self.run_argmax_on_y_true = run_argmax_on_y_true
     
     def compute(self,
@@ -37,32 +41,49 @@ class Metrics:
         y_pred and y_true must have shape like (B, 2)
         """
         metrics_value = []
-        y_pred = torch.argmax(y_pred, dim=-1)
         if self.run_argmax_on_y_true:
             y_true = torch.argmax(y_true, dim=-1)
+
+        for metric in self.metrics_onehot.values():
+            metrics_value.append(metric(y_pred, y_true).item())
+
+        y_pred = torch.argmax(y_pred, dim=-1)
         
         for metric in self.metrics.values():
             metrics_value.append(metric(y_pred, y_true).item())
         return np.array(metrics_value)
     
     def get_names(self) -> List[str]:
-        return list(self.metrics.keys())
+        return self.metrics_name
     
     def init_metrics(self) -> np.ndarray:
         return np.zeros(self.num_metrics)
     
     def to(self, device: torch.device) -> None:
+        for key in self.metrics_onehot.keys():
+            self.metrics_onehot[key] = self.metrics_onehot[key].to(device)
         for key in self.metrics.keys():
             self.metrics[key] = self.metrics[key].to(device)
 
+    def get_info(self, metrics_value: np.ndarray) -> str:
+        if len(metrics_value) != self.num_metrics:
+            raise ValueError(f'metrics_value doesnt have the same length as num_metrics.',
+                             f'{len(metrics_value) = }\t and \t{self.num_metrics = }')
+        
+        output = 'Metrics :\n'
+        for i, metric_name in enumerate(self.metrics_name):
+            output += f'{metric_name[:7]}\t: {metrics_value[i]:.2f}\n'
+        return output
+
 
 if __name__ == '__main__':
-    batch_size = 3
+    batch_size = 10
     num_classes = 3
     y_pred = torch.rand(size=(batch_size, num_classes))
     y_true = torch.randint(num_classes, size=(batch_size,))
-    print('y_true',y_true)
-    print('y_pred',y_pred)
+    print('y_true', y_true)
+    print('y_pred', y_pred)
     
     metrics = Metrics(num_classes=num_classes, run_argmax_on_y_true=False)
-    print(metrics.compute(y_pred, y_true))
+    metrics_value = metrics.compute(y_pred, y_true)
+    print(metrics.get_info(metrics_value))
