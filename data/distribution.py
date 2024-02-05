bd0025195c0dd254741b88217eb23673ba3d9cf2
Highdrien <adrien.zabban@outlook.fr>
2024-02-05 11:39:45 +0100

    add distribution

---
+++ data/distribution.py
@@ -0,0 +1,32 @@
+import os
+from typing import List
+
+import informations as info
+
+
+def get_distribution(mode: str,
+                     datapath: str=os.path.join('data', 'data_labo')
+                     ) -> List[float]:
+    datapath = os.path.join(datapath, f'{mode}_128')
+    labels_distribution = []
+    for label in info.LABELS:
+        num_data = 0
+        for bg in info.BACKGROUND:
+            num_data += len(os.listdir(os.path.join(datapath, label, bg)))
+        labels_distribution.append(num_data)
+
+    N = sum(labels_distribution)
+    for i in range(len(labels_distribution)):
+        labels_distribution[i] /= N
+    
+    return labels_distribution
+
+
+if __name__ == '__main__':
+    train = get_distribution(mode='train')
+    val = get_distribution(mode='val')
+    test = get_distribution(mode='test')
+
+    print('label, train, val, test')
+    for i in range(len(info.LABELS)):
+        print(f'{i + 1:2}: {train[i]:.2f} {val[i]:.2f} {test[i]:.2f}')
