7f2300328ee362bc973a0b9c43fb1fe5066d0c6f
Highdrien <adrien.zabban@outlook.fr>
2024-03-18 15:45:03 +0100

    ✨ add comments

--- data/distribution.py
+++ data/distribution.py
@@ -14,6 +14,17 @@ def get_distribution(mode: str,
                      datapath: str=os.path.join('data', 'data_labo'),
                      use_background: bool=True
                      ) -> np.ndarray[float]:
+    """
+    Get the distribution of data for each label.
+
+    Args:
+        mode (str): The mode of the data (e.g., 'train', 'val', 'test').
+        datapath (str, optional): The path to the data directory. Defaults to 'data/data_labo'.
+        use_background (bool, optional): Whether to include background data. Defaults to True.
+
+    Returns:
+        np.ndarray[float]: The distribution of data for each label.
+    """
     datapath = os.path.join(datapath, f'{mode}_256')
     labels_distribution = []
     for label in LABELS:
@@ -33,6 +44,16 @@ def get_distribution(mode: str,
 def plot_distrib(labels_distribution: list[int],
                  title: str='distribution.png'
                  ) -> None:
+    """
+    Plot the distribution of data for each label.
+
+    Args:
+        labels_distribution (list[int]): The distribution of data for each label.
+        title (str, optional): The title of the plot. Defaults to 'distribution.png'.
+
+    Returns:
+        None
+    """
     total_classes = len(labels_distribution)
     classes_index = range(1, total_classes + 1)
 
@@ -53,10 +74,22 @@ def plot_3distribution(distribution1: np.ndarray[float],
                        distribution3: np.ndarray[float],
                        title='distribution_train_val_test.png'
                        ) -> None:
+    """
+    Plot the comparison of three distributions.
+
+    Args:
+        distribution1 (np.ndarray[float]): The first distribution.
+        distribution2 (np.ndarray[float]): The second distribution.
+        distribution3 (np.ndarray[float]): The third distribution.
+        title (str, optional): The title of the plot. Defaults to 'distribution_train_val_test.png'.
+
+    Returns:
+        None
+    """
     classes_index = np.arange(1, 19)
     bar_width = 0.25
 
-    fig, ax = plt.subplots(figsize=(12, 6))
+    _, ax = plt.subplots(figsize=(12, 6))
 
     ax.bar(classes_index - bar_width, distribution1, bar_width, label='Distribution 1')
     ax.bar(classes_index, distribution2, bar_width, label='Distribution 2')
