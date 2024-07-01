939cf13ca5707b2b36c1053ea926b99641d4cc12
Clara <clarakar02@gmail.com>
2024-07-01 13:21:49 +0200

    debugging gradcam

--- src/dataloader/transforms.py
+++ src/dataloader/transforms.py
@@ -23,7 +23,7 @@ def get_transforms(transforms_config: EasyDict, mode: str) -> Compose:
     """
     transform = []
 
-
+    transform.append(Grayscale(num_output_channels=3))
 
     if mode == "train":
         if transforms_config.run_rotation:
