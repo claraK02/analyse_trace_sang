39a312084f630d6c8a477ee99f00e8232d17af7e
Clara <clarakar02@gmail.com>
2024-07-04 11:44:54 +0200

    false

--- src/gradcam.py
+++ src/gradcam.py
@@ -46,7 +46,7 @@ class GradCam:
         
         visualizations = np.zeros((batch_size, img_h, img_w, img_c))
         for b in range(batch_size):
-            rgb_img = utils.convert_tensor_to_rgb(image=image[b].cpu(), normelize=True)
+            rgb_img = utils.convert_tensor_to_rgb(image=image[b].cpu(), normelize=False)
             visualizations[b] = show_cam_on_image(rgb_img, grayscale_cam[b])
 
         return visualizations
@@ -76,7 +76,7 @@ class GradCam:
                                   image: Tensor,
                                   ) -> Tensor:
         """
-        Calculates the probability of each class in the given image using a the saliency mask.
+        Calculates the probability of each class in the given image using the saliency mask.
 
         Args:
             model (FineTuneResNet): The model used for prediction.
@@ -102,7 +102,7 @@ if __name__ == '__main__':
     from src.model.finetune_resnet import get_finetuneresnet
 
     batch_size = 2
-    x = torch.rand(batch_size, 1, 256, 256)
+    x = torch.rand(batch_size, 3, 256, 256)
     for i in range(batch_size):
         xi, _ = utils.get_random_img(image_type='torch')
         x[i] = xi
